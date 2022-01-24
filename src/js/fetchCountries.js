e490a658f6048a67a7ba9a16b9eb4c0f388966fc
Volodymyr Bendyk <vvbendyk@gmail.com>
2022-01-24 12:07:36 +0200

    24.01

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -3,6 +3,10 @@ const fields = 'fields=name,capital,population,flags,languages'
 
 export function fetchCountries(name) {
   return fetch(`${BASE_URL}${name}?${fields}`)
-    .then(response => response.json())
+    .then(response => {
+      if (!response.ok) {
+      throw new Error(response.status)
+    }
+      return response.json()})
     .catch(error => console.log(error))
 }
